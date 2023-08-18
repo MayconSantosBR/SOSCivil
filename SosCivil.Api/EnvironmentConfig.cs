52fb78dd11dfa7e62512bf8a8d8018946998edff
MayconSantosBR <maycon.wb.santos@gmail.com>
2023-08-18 20:14:14 -0300

    Added environment config class

---
+++ SosCivil.Api/EnvironmentConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace SosCivil.Api
+{
+    public static class EnvironmentConfig
+    {
+        //Add here the main options, like DbContext connection string.
+        public static class Context
+        {
+            public static string Main = "teste@teste";
+        }
+    }
+}
