ab172df5c20594cf60fd8f11e3db92a133f6b483
MayconSantosBR <maycon.wb.santos@gmail.com>
2023-08-18 21:15:57 -0300

    Adicionado projeto core para compartilhamento de código, adicionado novos enums para tipo de pessoa e status

---
+++ SosCivil.Core/Data/Enums/StatusEnum.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace SosCivil.Api.Data.Enums
+{
+    public enum StatusEnum
+    {
+        [Description("Criado")]
+        Created = 0,
+
+        [Description("Arquivado")]
+        Backlog = 1,
+
+        [Description("Em análise")]
+        Analysis = 2,
+
+        [Description("Aguardando aprovação")]
+        WaitingApproval = 3,
+
+        [Description("Aprovado")]
+        Approved = 4,
+
+        [Description("Em transporte")]
+        Shipping = 5,
+
+        [Description("Entregue")]
+        Delivered = 6,
+
+        [Description("Cancelado")]
+        Canceled = 7,
+
+        [Description("Rejeitado")]
+        Rejected = 8,
+    }
+}
---
+++ SosCivil.Core/Data/Enums/TipoPessoaEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SosCivil.Core.Data.Enums
+{
+    public enum TipoPessoaEnum
+    {
+        [Description("Pessoa física")]
+        Fisica = 0,
+
+        [Description("Pessoa jurídica")]
+        Juridica = 1,
+
+        [Description("Usuário padrão")]
+        Usuario = 2,
+
+        [Description("Administrador")]
+        Administrador = 3
+    }
+}
