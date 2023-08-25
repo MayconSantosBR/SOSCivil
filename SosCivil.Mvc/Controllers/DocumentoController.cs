1d2961f19dc3f22ce6e10886fda1c8b4bf15bcc8
DESKTOP-N13K4BE\Gabriel <gabrielgsartori@outlook.com>
2023-08-25 16:22:52 -0300

    Novo menu e novas controllers

---
+++ SosCivil.Mvc/Controllers/DocumentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SosCivil.Mvc.Controllers
+{
+    public class DocumentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
