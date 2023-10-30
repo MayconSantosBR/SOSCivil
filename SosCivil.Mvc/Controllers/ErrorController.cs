d76966427601c77c8a10bfa680e88d32fff74cf7
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2023-10-30 20:32:48 -0300

    Ajustes Front e tratativas de erros

---
+++ SosCivil.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SosCivil.Mvc.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly Dictionary<int, string> ErrorViewMap = new Dictionary<int, string>
+        {
+            { 400, "/Views/Error/400.cshtml" },
+            { 401, "/Views/Error/401.cshtml" },
+            { 403, "/Views/Error/403.cshtml" },
+            { 404, "/Views/Error/404.cshtml" },
+            { 500, "/Views/Error/500.cshtml" },
+            { 502, "/Views/Error/503.cshtml" },
+            { 504, "/Views/Error/504.cshtml" },
+        };
+
+        [Route("Error/{statusCode}")]
+        public IActionResult HandleError(int? statusCode)
+        {
+            if (statusCode.HasValue && ErrorViewMap.ContainsKey(statusCode.Value))
+            {
+                return View(ErrorViewMap[statusCode.Value]);
+            }
+
+            return View("/Views/Error/GenericError.cshtml");
+        }
+    }
+}
