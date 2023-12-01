f840ae435bd49ec3aedef889f9ab33c0750b3559
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2023-12-01 14:47:01 -0300

    Testes e ajustes na tabela solicitações

---
+++ SosCivil.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SosCivil.Core.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string GetDescription<T>(this T enumValue)
+            where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                return null;
+
+            var description = enumValue.ToString();
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attrs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    description = ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return description;
+        }
+    }
+}
