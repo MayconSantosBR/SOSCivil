4753af01f2d246b68524010f906c5dee7e43675f
DESKTOP-N13K4BE\Gabriel <gabrielgsartori@outlook.com>
2023-12-01 22:03:55 -0300

    ajuste pattern

--- SosCivil.Mvc/Configuration/WebAppConfig.cs
+++ SosCivil.Mvc/Configuration/WebAppConfig.cs
@@ -31,7 +31,7 @@
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Catalog}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
         }
