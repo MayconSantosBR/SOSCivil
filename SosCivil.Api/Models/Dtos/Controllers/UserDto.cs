cf5a20944490ef10d8901701182cd3ae03bf952b
MayconSantosBR <maycon.wb.santos@gmail.com>
2023-12-05 21:52:17 -0300

    Adicionado CRUD para User

---
+++ SosCivil.Api/Models/Dtos/Controllers/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SosCivil.Api.Models.Dtos.Controllers
+{
+    public class UserDto
+    {
+        public long PersonId { get; set; }
+
+        public string Username { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
