c37014e87e58d210c113e26963d1e1ae6feef237
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2023-12-07 02:12:03 -0300

    Migration de jaguara

--- SosCivil.Api/Data/Entities/Occurrence.cs
+++ SosCivil.Api/Data/Entities/Occurrence.cs
@@ -13,6 +13,6 @@ namespace SosCivil.Api.Data.Entities
         public User User { get; set; }
         public StatusEnum Status { get; set; }
         public long RequestItemId { get; set; }
-        public ICollection<RequestItem> RequestItem { get; set; }
+        public RequestItem RequestItem { get; set; }
     }
 }
