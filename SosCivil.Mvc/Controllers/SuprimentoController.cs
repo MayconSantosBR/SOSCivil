681aa10468c0b9003fe9f45c761f8499b02a11c5
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2023-12-07 01:13:32 -0300

    Update MVC

--- SosCivil.Mvc/Controllers/SuprimentoController.cs
+++ SosCivil.Mvc/Controllers/SuprimentoController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SosCivil.Api.Data.Enums;
+using SosCivil.Core.Data.Enums;
+using SosCivil.Mvc.Models;
 
 namespace SosCivil.Mvc.Controllers
 {
@@ -6,11 +9,64 @@ namespace SosCivil.Mvc.Controllers
     {
         public IActionResult Index()
         {
+            var suprimentos = new List<Suprimento>
+            {
+                new Suprimento {
+                    Id = 1,
+                    Nome = "Água",
+                    Descricao = "Água potável",
+                    Quantidade = 10,
+                    QuantidadeTotal = 100,
+                    UnidadeDeMedida = UnityOfMeasurementEnum.Liters,
+                },
+                new Suprimento
+                {
+                    Id = 2,
+                    Nome = "Alimento",
+                    Descricao = "Alimento não perecível",
+                    Quantidade = 10,
+                    QuantidadeTotal = 100,
+                    UnidadeDeMedida = UnityOfMeasurementEnum.Kilograms,
+                },
+                new Suprimento
+                {
+                    Id = 3,
+                    Nome = "Remédio",
+                    Descricao = "Remédio para dor de cabeça",
+                    Quantidade = 10,
+                    QuantidadeTotal = 100,
+                    UnidadeDeMedida = UnityOfMeasurementEnum.Unit,
+                }
+            };
+            ViewData["Suprimentos"] = suprimentos;
             return View();
         }
-        public IActionResult RegistrationSupriments()
+
+        public IActionResult Novo()
         {
-            return View();
+            var model = new Suprimento();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Novo(Solicitacao model)
+        {
+            return View(model);
+        }
+
+        public IActionResult Editar(long id)
+        {
+            var model = new Suprimento
+            {
+                Id = 1,
+                Nome = "Água",
+                Descricao = "Água potável",
+                Quantidade = 10,
+                QuantidadeTotal = 100,
+                UnidadeDeMedida = UnityOfMeasurementEnum.Liters,
+            };
+            return View("Novo", model);
         }
     }
 }
+
--- SosCivil.Mvc/Models/Suprimento.cs
+++ SosCivil.Mvc/Models/Suprimento.cs
@@ -1,4 +1,5 @@
 ﻿using SosCivil.Api.Data.Enums;
+using SosCivil.Core.Data.Enums;
 using SosCivil.Mvc.Controllers;
 
 namespace SosCivil.Mvc.Models
@@ -6,6 +7,10 @@ namespace SosCivil.Mvc.Models
     public class Suprimento
     {
         public int Id { get; set; }
-        public string NomeItem { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public UnityOfMeasurementEnum UnidadeDeMedida { get; set; }
+        public decimal Quantidade { get; set; }
+        public decimal QuantidadeTotal { get; set; }
     }
 }
